97bc6a2e17f9bc2afc9cef5a0ca277b487477191
Slavena Vasileva <svasileva@asteasolutions.com>
2015-02-02 21:34:41 +0200

    Added mallet gate pipe and gate classifier.

---
+++ short-sentiment-core/src/main/java/core/CharSequence2StemmedTokenSequence.java
@@ -0,0 +1,47 @@
+package core;
+
+import gate.util.GateException;
+
+import java.io.IOException;
+import java.io.Serializable;
+
+import cc.mallet.pipe.Pipe;
+import cc.mallet.types.Instance;
+import cc.mallet.types.TokenSequence;
+
+public class CharSequence2StemmedTokenSequence extends Pipe implements Serializable {
+	/**
+	 *
+	 */
+	private static final long serialVersionUID = 1L;
+	GateProcessor processor;
+
+	public CharSequence2StemmedTokenSequence() {
+		processor = new GateProcessor();
+		try {
+			processor.initialize();
+		} catch (GateException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+	@Override
+	public Instance pipe(Instance carrier) {
+		CharSequence charSequence = (CharSequence) carrier.getData();
+		String dataString = charSequence.toString();
+		String[] tokens = {};
+		try {
+			tokens = processor.processString(dataString);
+		} catch (GateException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		TokenSequence ts = new TokenSequence(tokens);
+		carrier.setData(ts);
+		return carrier;
+	}
+}
