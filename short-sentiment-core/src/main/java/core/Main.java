cc471652fd035b2e0fb1a9d420da4cc55434b927
LachezarKozhuharov <lacho.kozhuharov@gmail.com>
2015-02-04 22:34:40 +0200

    Fixed Main.java. Most of the classifier accuracy calculations are temporary disbled.

--- short-sentiment-core/src/main/java/core/Main.java
+++ short-sentiment-core/src/main/java/core/Main.java
@@ -5,10 +5,7 @@ import java.util.List;
 import cc.mallet.classify.Classification;
 import cc.mallet.classify.Classifier;
 import core.classifier.MaxEntClassifier;
-import core.classifier.NaiveBayesBigramGateClassifier;
 import core.classifier.NaiveBayesClassifier;
-import core.classifier.NaiveBayesGateClassifier;
-import core.classifier.SVMClassifier;
 import core.classifier.TweetClassifier;
 
 public class Main {
@@ -16,13 +13,13 @@ public class Main {
 		TweetsGenerator generator = new TweetsGenerator("full-corpus.csv");
 
 		try {
-//			List<Tweet> tweets = generator.loadTweets();
-//			System.out.println("All tweets");
-//			mesureAccuracy(tweets);
-//			tweets = generator.getSubjectiveTweets();
-//			System.out.println("########################");
-//			System.out.println("Subjective tweets");
-//			mesureAccuracy(tweets);
+			List<Tweet> tweets = generator.loadTweets();
+			System.out.println("All tweets");
+			measureAccuracy(tweets);
+			tweets = generator.getSubjectiveTweets();
+			System.out.println("########################");
+			System.out.println("Subjective tweets");
+			measureAccuracy(tweets);
 
 			TweetClassifier classifier = new NaiveBayesClassifier(tweets, 0.9);
 			Classifier naiveBayes = classifier.getMalletClassifier();
@@ -38,45 +35,44 @@ public class Main {
 		}
 	}
 
-	public static void mesureAccuracy(final List<Tweet> tweets) {
-		AverageClassifierAccuracy aca = new AverageClassifierAccuracy(new EvaluationClassifierBuilder() {
-			@Override
-			public TweetClassifier buildClassifier() {
-				return new NaiveBayesClassifier(tweets, 0.9);
-			}
-		});
+	public static void measureAccuracy(final List<Tweet> tweets) {
+		int samples = 5;
+		AverageClassifierAccuracy aca = new AverageClassifierAccuracy(
+				new EvaluationClassifierBuilder() {
+					public TweetClassifier buildClassifier() {
+						return new NaiveBayesClassifier(tweets, 0.9);
+					}
+				}, samples);
 		System.out.println("NaiveBayes: " + aca.getAverageAccuracy());
 
-		aca = new AverageClassifierAccuracy(new EvaluationClassifierBuilder() {
-			@Override
-			public TweetClassifier buildClassifier() {
-				return new NaiveBayesGateClassifier(tweets, 0.9);
-			}
-		});
-		System.out.println("NaiveBayesGate: " + aca.getAverageAccuracy());
+//		aca = new AverageClassifierAccuracy(new EvaluationClassifierBuilder() {
+//			public TweetClassifier buildClassifier() {
+//				return new NaiveBayesGateClassifier(tweets, 0.9);
+//			}
+//		}, samples);
+//		System.out.println("NaiveBayesGate: " + aca.getAverageAccuracy());
+
+//		aca = new AverageClassifierAccuracy(new EvaluationClassifierBuilder() {
+//			public TweetClassifier buildClassifier() {
+//				return new NaiveBayesBigramGateClassifier(tweets, 0.9);
+//			}
+//		}, samples);
+//		System.out
+//				.println("NaiveBayesBigrameGate: " + aca.getAverageAccuracy());
 
 		aca = new AverageClassifierAccuracy(new EvaluationClassifierBuilder() {
-			@Override
-			public TweetClassifier buildClassifier() {
-				return new NaiveBayesBigramGateClassifier(tweets, 0.9);
-			}
-		});
-		System.out.println("NaiveBayesBigrameGate: " + aca.getAverageAccuracy());
-
-		aca = new AverageClassifierAccuracy(new EvaluationClassifierBuilder() {
-			@Override
 			public TweetClassifier buildClassifier() {
 				return new MaxEntClassifier(tweets, 0.9);
 			}
-		});
+		}, samples);
 		System.out.println("Max ent: " + aca.getAverageAccuracy());
 
-		aca = new AverageClassifierAccuracy(new EvaluationClassifierBuilder() {
-			@Override
-			public TweetClassifier buildClassifier() {
-				return new SVMClassifier(tweets, 0.9);
-			}
-		});
-		System.out.println("SVM: " + aca.getAverageAccuracy());
+		// aca = new AverageClassifierAccuracy(new EvaluationClassifierBuilder()
+		// {
+		// public TweetClassifier buildClassifier() {
+		// return new SVMClassifier(tweets, 0.9);
+		// }
+		// });
+		// System.out.println("SVM: " + aca.getAverageAccuracy());
 	}
 }
